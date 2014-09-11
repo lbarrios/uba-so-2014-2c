124b3110d2370718ece4afe7e7bb1f892885e271
gbenegas <gsbenegas@gmail.com>
2014-09-11 01:39:40 -0300

    avanzando con SchedLottery

--- tp1/codigo/sched_lottery.cpp
+++ tp1/codigo/sched_lottery.cpp
@@ -2,23 +2,81 @@
 
 using namespace std;
 
+#define DEBUG(var) cout << #var << ": " << var << endl;
+
 SchedLottery::SchedLottery(vector<int> argn) {
-  // FCFS recibe la cantidad de cores.
+    quantum = argn[0];
+    seed = argn[1];
+    srand(seed);
+    total_tickets = 0;
 }
 
 SchedLottery::~SchedLottery() {
 }
 
 void SchedLottery::load(int pid) {
-  load(pid,0);
-}
-
-void SchedLottery::load(int pid,int deadline) {
+    tickets.push_back(ticket(pid, 1));
+    total_tickets++;
 }
 
 void SchedLottery::unblock(int pid) {
+    // no hacemos nada
+}
+
+int SchedLottery::tickets_index(int pid) {
+    for(int i = 0; i < tickets.size(); i++) {
+        if (tickets[i].pid == pid) return i;
+    }
+    throw invalid_argument("el pid no esta en tickets");
+}
+
+void SchedLottery::compensa(int pid) {
+    int compensation = int(float(tick_number+1) / (float)quantum);
+    tickets[tickets_index(pid)].count = compensation;
+    total_tickets += compensation-1;
+}
+
+void SchedLottery::desaloja(int pid) {
+    if (pid != IDLE_TASK) {
+        tickets.erase(tickets.begin() + tickets_index(pid));
+        total_tickets--;
+    }
+}
+
+int SchedLottery::run_lottery() {
+    tick_number = 0;
+    if (total_tickets == 0) {
+        return IDLE_TASK;
+    }
+
+    int winner = rand() % total_tickets;
+    int i = 0;
+    int accum = 0;
+    while (accum < winner) {
+        accum += tickets[i].count;
+        i++;
+    }
+    if (i >= tickets.size())
+        throw invalid_argument("nadie es duenio de ese ticket");
+
+    total_tickets -= tickets[i].count - 1;
+    tickets[i].count = 1;
+    return tickets[i].pid;
 }
 
 int SchedLottery::tick(int cpu, const enum Motivo m) {
-  return 0;
+    if (m == TICK) {
+        tick_number++;
+        if (tick_number >= quantum) {
+            // desalojo a la tarea
+        } else 
+            return current_pid(cpu);
+
+    } else if (m == BLOCK) {
+        compensa(current_pid(cpu));
+    } else if (m == EXIT) {
+        desaloja(current_pid(cpu));
+
+    }
+    return run_lottery();
 }
--- tp1/codigo/sched_lottery.h
+++ tp1/codigo/sched_lottery.h
@@ -2,17 +2,36 @@
 #define __SCHED_LOT__
 
 #include "basesched.h"
+#include <vector>
+#include <stdexcept>
+#include <stdlib.h>
+#include <iostream>
 
+using namespace std;
+
+struct ticket {
+    int pid;
+    int count;
+    ticket(int pid, int count): pid(pid), count(count) {};
+};
 
 class SchedLottery : public SchedBase {
-  public:
+public:
     SchedLottery(std::vector<int> argn);
         ~SchedLottery();
     virtual void load(int pid);
-    virtual void load(int pid, int deadline);
     virtual void unblock(int pid);
     virtual int tick(int cpu, const enum Motivo m);
-
+private:
+    int quantum;
+    int seed;
+    vector<ticket> tickets;
+    int tick_number;
+    int run_lottery();
+    void compensa(int);
+    void desaloja(int);
+    int tickets_index(int);
+    int total_tickets;
 };
 
 #endif
